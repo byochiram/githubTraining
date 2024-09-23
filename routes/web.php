82a031ea8ca4f6cad835fb23f6f2ff5a36a95291
itsekrie <fahriantekrie@students.undip.ac.id>
2024-09-23 16:04:45 +0700

    tutormode

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Okky', function(){
+    return view('okky');
+})->name('okky');
+
+Route::get('/Alba', function(){
+    return view('alba');
+})->name('alba');
+
+Route::get('/Ucup', function(){
+    return view('ucup');
+})->name('ucup');
+
+Route::get('/Demina', function(){
+    return view('demina');
+})->name('demina');
+
+Route::get('/Keisya', function(){
+    return view('keisya');
+})->name('keisya');
+
+Route::get('/Ida', function(){
+    return view('ida');
+})->name('ida');
+
+Route::get('/Aulya', function(){
+    return view('aulya');
+})->name('aulya');
